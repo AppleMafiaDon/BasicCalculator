17af372dca75b4d35edb6acd6fdefae6a90a5af2
Taylor Apple <apple_maffia3@msn.com>
2018-10-28 17:40:19 -0700

    Committing changes to the calculator translating from single value to in-fix calculator. Added "(" and ")" buttons, transformed the button select commands, transformed the calculate from simple calculator into a two stack in-fix calculator.

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -1,71 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using System.Text.RegularExpressions;
+
 
 namespace Calculator
 {    
     public partial class MainPage : ContentPage
-    {    
-		int currentState = 1;
-		string mathOperator;
-		double firstNumber, secondNumber;
-
+    {
         public MainPage ()
         {
             InitializeComponent ();
             OnClear(this, null);
         }
 
-		void OnSelectNumber(object sender, EventArgs e)
+		void OnSelectButton(object sender, EventArgs e)
 		{
-			Button button = (Button)sender;
+            Button button = (Button)sender;
 			string pressed = button.Text;
 
-			if (this.resultText.Text == "0" || currentState < 0) {
-				this.resultText.Text = "";
-				if (currentState < 0)
-					currentState *= -1;
-			}
-
-			this.resultText.Text += pressed;
-
-			double number;
-			if (double.TryParse(this.resultText.Text, out number)) {
-				this.resultText.Text = number.ToString("N0");
-				if (currentState == 1) {
-					firstNumber = number;
-				} else {
-					secondNumber = number;
-				}
-			}
-		}
-
-		void OnSelectOperator(object sender, EventArgs e)
-		{
-			currentState = -2;
-			Button button = (Button)sender;
-			string pressed = button.Text;
-			mathOperator = pressed;
+            this.equation.Text += pressed + " ";
 		}
 
 		void OnClear(object sender, EventArgs e)
 		{
-			firstNumber = 0;
-			secondNumber = 0;
-			currentState = 1;
-			this.resultText.Text = "0";
+            this.equation.Text = "";
+            this.resultText.Text = "0";
 		}
 
-		void OnCalculate(object sender, EventArgs e)
-		{
-			if (currentState == 2)
+        void OnCalculate(object sender, EventArgs e)
+        {
+            string eq = this.equation.Text;
+            if (Regex.Matches(eq, @"[a-zA-Z]").Count > 1)
+            { 
+                this.resultText.Text = "Error: Invalid Equation; Letters are present";
+                return;
+            }
+            else if(Regex.Matches(eq, @"(").Count != Regex.Matches(eq, @")").Count)
             {
-                var result = SimpleCalculator.Calculate(firstNumber, secondNumber, mathOperator);
+                this.resultText.Text = "Error: Invalid Equation; Wrong number of parenthesis";
+                return;
+            }
 
-                this.resultText.Text = result.ToString();
-                firstNumber = result;
-                currentState = -1;
-			}
+            var result = SimpleCalculator.Calculate(this.equation.Text);
+            if (result == -1)
+            {
+                this.resultText.Text = "Error: Invalid Equation; Wrong number of operators or arguments";
+                return;
+            }
+            this.resultText.Text = result.ToString();
 		}
 	}
 }
--- Calculator/SimpleCalculator.cs
+++ Calculator/SimpleCalculator.cs
@@ -8,27 +8,61 @@ namespace Calculator
 {
     public static class SimpleCalculator
     {
-        public static double Calculate(double value1, double value2, string mathOperator)
+        public static double Calculate(string input)
         {
-            double result = 0;
-
-            switch (mathOperator)
+            double value = 0;
+            var precedence = new Dictionary<string, int> { { "(", 0 }, { "*", 1 }, { "/", 1 }, { "+", 2 }, { "-", 2 }, { ")", 3 } };
+            Stack<string> operators = new Stack<string>();
+            Stack<double> operands = new Stack<double>();
+            string[] eq = input.Split(' ');
+            foreach (string term in eq)
             {
-                case "÷":
-                    result = value1 / value2;
+                if (double.TryParse(term, out value))
+                {
+                    operands.Push(value);
+                }
+                else if (precedence.ContainsKey(term))
+                {
+                    bool keepLooping = true;
+                    while (keepLooping && operators.Count > 0 && precedence[term] > precedence[operators.Peek()])
+                    {
+                        switch (operators.Peek())
+                        {
+                            case "+":
+                                operands.Push(operands.Pop() + operands.Pop());
+                                break;
+                            case "-":
+                                operands.Push(-operands.Pop() + operands.Pop());
+                                break;
+                            case "*":
+                                operands.Push(operands.Pop() * operands.Pop());
+                                break;
+                            case "/":
+                                var divisor = operands.Pop();
+                                operands.Push(operands.Pop() / divisor);
+                                break;
+                            case "(":
+                                keepLooping = false;
+                                break;
+                        }
+                        if (keepLooping)
+                            operators.Pop();
+                    }
+                    if (term == ")")
+                        operators.Pop();
+                    else
+                        operators.Push(term);
                     break;
-                case "×":
-                    result = value1 * value2;
-                    break;
-                case "+":
-                    result = value1 + value2;
-                    break;
-                case "-":
-                    result = value1 - value2;
-                    break;
+                }
+                else
+                {
+                    return -1;
+                }
             }
+            if (operators.Count > 0 || operands.Count > 1)
+                return -1;
 
-            return result;
+            return operands.Pop();
         }
     }
 }
